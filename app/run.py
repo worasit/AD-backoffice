19b8ecca85b21b04bb637f2d229148f08bebfc2c
worasit daimongkol <worasit.dmk501@gmail.com>
2018-11-27 20:01:05 +0700

    Added example route which render the Jinja2 template.

--- app/run.py
+++ app/run.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
+env = Environment(
+    loader=PackageLoader('app', 'templates'),
+    autoescape=select_autoescape(['html'])
+)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html',
+                           message='This is the draft version of AD Manager application')
 
 
 if __name__ == '__main__':
